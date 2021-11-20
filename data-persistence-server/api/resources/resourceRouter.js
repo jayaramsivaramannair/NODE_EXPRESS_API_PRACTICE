4b5d6cce96d2d35c605e3b1ce8f3578c60451400
Jayaram Nair <jayaramsivaramannair@gmail.com>
2021-11-19 19:06:56 -0500

    Document APIs in Swagger for fetching all resources and a resource with specific ID

--- data-persistence-server/api/resources/resourceRouter.js
+++ data-persistence-server/api/resources/resourceRouter.js
@@ -2,6 +2,60 @@ const resources = require('./resourceModel.js');
 
 const router = require('express').Router();
 
+/** 
+ * @swagger
+ * 
+ * components: 
+ *   schemas:
+  *    resources:
+    *     type: object
+    *     required:
+    *       - resource-name
+    *     properties:
+    *       resource-id: 
+    *        type: integer
+    *        description: Auto-incrementing ID for each resource
+    *       resource-name:
+    *         type: string
+    *         description: Name of the resource
+    *       resource-description: 
+    *         type: string
+    *         description: Description of the resource
+    *     example:
+    *      resource-id: 1
+    *      resource-name: "The Ring"
+    *      resource-description: "One Ring to Rule Them All!"
+    *      
+*/
+
+/**
+ * @swagger
+ * tags: 
+ *   name: Resources
+ *   description: API for managing resources
+ * 
+ */
+
+/**
+ * @swagger
+ * /api/resources:
+ *    get:
+ *      summary: Returns a list of all resources
+ *      tags: [Resources]
+ *      responses:
+ *        200:
+ *         description: An array of resources  (see example below)
+ *         content:
+ *           application/json:
+ *            schema:
+ *              type: array 
+ *              items:
+ *                $ref: '#/components/schemas/resources'
+ *                
+ *              
+ *        
+ */
+
 
 router.get('/', async (req, res, next) => {
   try {
@@ -14,9 +68,34 @@ router.get('/', async (req, res, next) => {
   } catch (err) {
     res.status(500).send(`${err.message} ${err.stack}`);
   }
-  
-  next();
+
 })
+
+/**
+ * @swagger
+ * /api/resources/{id}:
+ *   get:
+ *    summary: Get the resource by specified resource-id
+ *    tags: [Resources]
+ *    parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: id of the resource
+ *    responses: 
+ *        200:
+ *         description: Listing of resource with the specified resource-id
+ *         content:
+ *           application/json:
+ *              schema: 
+ *                $ref: '#/components/schemas/resources'  
+ *        404:
+ *          description: Resource with the specified ID does not exist! 
+ * 
+ */
+
 
 
 router.get('/:id', async (req, res, next) => {
@@ -30,8 +109,6 @@ router.get('/:id', async (req, res, next) => {
   } catch (err) {
     res.status(500).send(`${err.message} ${err.stack}`);
   }
-
-  next();
 })
 
 module.exports = router;
